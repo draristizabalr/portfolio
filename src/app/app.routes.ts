1cfe0558f5e9669871e567d3b821e9af132e7c51
draristizabalr <draristizabalr@unal.edu.co>
2025-08-02 15:25:38 -0500

    Primera versión del template del portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home'),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about'),
+  },
+  {
+    path: 'skills',
+    loadComponent: () => import('./pages/skills/skills'),
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./pages/projects/projects'),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact'),
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
