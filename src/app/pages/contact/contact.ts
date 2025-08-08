30d03ece5836465ae58de4fc8c3d8d605422320d
draristizabalr <draristizabalr@unal.edu.co>
2025-08-08 16:05:00 -0500

    Ahora el front envía un correo electrónico de forma automática en la página de Contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,14 +1,19 @@
-import { JsonPipe } from '@angular/common';
-import { Component, computed, effect, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { Component, effect, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { ErrorContactFormPipe } from './error-contact-form.pipe';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [ReactiveFormsModule, ErrorContactFormPipe],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
 })
 export default class ContactComponent {
   // Injects
@@ -17,30 +22,36 @@ export default class ContactComponent {
   // Form
   contactForm = this.fb.group({
     name: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-    message: ['', [Validators.required]]
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+      ],
+    ],
+    message: ['', [Validators.required]],
   });
 
   // Signals
   invalidForm = signal(this.contactForm.invalid && this.contactForm.touched);
   errors = signal<ValidationErrors | null>(null);
+  emailSent = signal(false);
 
   constructor() {
     effect(() => {
       console.log(this.errors());
-    })
+    });
   }
 
-  onSubmit() {
+  onSubmit(event: Event) {
     this.contactForm.markAllAsTouched();
     this.invalidForm.set(this.contactForm.invalid && this.contactForm.touched);
     if (this.contactForm.invalid) {
       this.setErrors();
-      return
-    };
+      return;
+    }
 
-    const { name, email, message } = this.contactForm.value;
-    console.table({ name, email, message });
+    this.sendEmail(event);
   }
 
   setErrors() {
@@ -51,7 +62,29 @@ export default class ContactComponent {
       }
       return acc;
     }, {} as ValidationErrors);
-    console.log(errors);
+
     this.errors.set(errors);
+  }
+
+  sendEmail(event: Event) {
+    const serviceID = 'default_service';
+    const templateID = 'template_1bkg1kd';
+
+    emailjs
+      .sendForm(serviceID, templateID, event.target as HTMLFormElement, {
+        publicKey: 'sL7FGBqQ83qPS9_qR',
+      })
+      .then(
+        () => {
+          this.emailSent.set(true);
+          this.contactForm.reset();
+          setTimeout(() => {
+            this.emailSent.set(false);
+          }, 4000);
+        },
+        (error) => {
+          console.log('Error: ', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 }
